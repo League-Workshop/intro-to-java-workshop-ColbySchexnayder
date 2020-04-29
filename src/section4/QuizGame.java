fc6925e25e43124f3c8d374e2bd694b17668428a
Colby <Colby@192.168.86.46>
2020-04-28 19:57:57 -0700

    QuizGame

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,9 +1,26 @@
 package section4;
 
+import java.util.Random;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
+	/*	int num = 0;
+		num = num + 1; //increment
+		num += 1;
+		num++;
 		
+		num = num - 1; // decrement
+		num -= 1;
+		num--;
+		
+		Random rand = new Random();
+		num = rand.nextInt(101);
+		System.out.println(num);
+		num = rand.nextInt(101);
+		System.out.println(num);
+		num = new Random().nextInt(4);
+		//*/
 		// 1.  Create a variable to hold the user's score 
 		
 		// 2.  Ask the user a question 
